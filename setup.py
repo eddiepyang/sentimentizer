379940dfc121e2838d7d691e163b3317fc472e32
epy <eddiepyang@gmail.com>
2021-03-07 11:26:37 -0500

    refactored torch model

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 
     install_requires=[
         "numpy", "pandas", "torch", 
-        "seaborn", "spacy"
+        "seaborn", "spacy", "scikit-learn", 'jsonlines'
     ],
 
     package_data={
