78d718f6fdd81d0ec53ba2bfeaac60a651f04ab6
epy <eddiepyang@gmail.com>
2021-03-21 11:45:10 -0400

    included code for Trainer class

--- yelp_nlp/rnn/train.py
+++ yelp_nlp/rnn/train.py
@@ -2,42 +2,37 @@ import torch
 from torch import optim
 from torch import nn
 from torch.utils.data import DataLoader
+from dataclasses import dataclass
+
 import time
 import numpy as np
 from data import id_to_glove, CorpusData
 from model import RNN
 
 
-def fit(
-    model: RNN,
-    loss_function: object,
-    opitmizer: object,
-    dataclass: CorpusData,
-    batch_size: int,
-    epochs: int,
+@dataclass
+class Trainer:
+    """Trainer class helps with creating the data loader,
+    tracking the torch optimizer and model fitting"""
+    loss_function: object
+    optimizer: optim.Adam
+    dataclass: CorpusData
+    batch_size: int
+    epochs: int
+    workers: int
     device: str
-):
 
-    start = time.time()
-    model.train()
-
-    epoch_count = 0
-    losses = []
-    train_loader = DataLoader(dataclass, batch_size=batch_size)
-
-    def train_epoch():
-
-        nonlocal train_loader, losses
+    def train_epoch(self, model):
 
         i = 0
-        n = len(dataclass)
+        n = len(self.dataclass)
 
-        for j, (sent, target) in enumerate(train_loader):
+        for j, (sent, target) in enumerate(self.train_loader):
 
-            optimizer.zero_grad()
-            sent, labels = sent.long().to(device), target.float().to(device)
+            self.optimizer.zero_grad()
+            sent, labels = sent.long().to(self.device), target.float().to(self.device)
             log_probs = model(sent)
-            loss = loss_function(log_probs, labels.to(device))
+            loss = self.loss_function(log_probs, labels.to(self.device))
 
             # gets graident
             loss.backward()
@@ -50,71 +45,112 @@ def fit(
             )
 
             # updates with new gradient
-            optimizer.step()
+            self.optimizer.step()
 
             i += len(labels)
-            losses.append(loss.item())
-            if i % (batch_size*100) == 0:
-                print(f"""{i/n:.2f} of rows completed in {j+1} cycles, current loss at {np.mean(losses[-30:]):.4f}""")  # noqa: E501
+            self.losses.append(loss.item())
+            if i % (self.batch_size*100) == 0:
+                print(f"""{i/n:.2f} of rows completed in {j+1} cycles, current loss at {np.mean(self.losses[-30:]):.4f}""")  # noqa: E501
 
-    print('fitting model...')
+    def fit(self, model):
 
-    for epoch in range(epochs):
+        self.train_loader: DataLoader = DataLoader(
+            dataset=self.dataclass,
+            batch_size=self.batch_size,
+            num_workers=self.workers
+        )
+        model.to(self.device)
+        start = time.time()
+        model.train()
 
-        train_epoch()
+        epoch_count = 0
+        self.losses = []
 
-        epoch_count += 1
-        print(f'epoch {epoch_count} completed')
-    print(f'model fitting completed, {time.time()-start:.0f} seconds passed')
-    return losses
+        print('fitting model...')
+
+        for epoch in range(self.epochs):
+
+            self.train_epoch(model)
+
+            epoch_count += 1
+            print(f'epoch {epoch_count} completed')
+        print(f'model fitting completed, {time.time()-start:.0f} seconds passed')  # noqa: E501
 
 
-if __name__ == '__main__':
+def main():
 
-    fpath = '../../data/archive.zip'
-    fname = 'yelp_academic_dataset_review.json'
-    abs_path = '/projects/yelp_nlp/data/'
-    state_path = '../../data/model_weight.pt'
-    device = 'cpu'
-    batch_size = 200
-    input_len = 200
-    n_epochs = 2
-    stop = 1000
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--fpath', default='../../data/archive.zip')
+    parser.add_argument('--fname', default='yelp_academic_dataset_review.json')
+    parser.add_argument('--abs_path', default='/projects/yelp_nlp/data/')
+    parser.add_argument('--state_path', default='../../data/model_weight.pt')
+    parser.add_argument('--device', default='cpu', help='run model on cuda or cpu')  # noqa: E501
+    parser.add_argument('--batch_size', type=int, default=200)
+    parser.add_argument('--input_len', type=int, default=200)
+    parser.add_argument(
+        '--n_epochs',
+        type=int,
+        default=2
+    )
+    parser.add_argument(
+        '--stop',
+        type=int,
+        default=1000,
+        help='how many lines to load'
+    )
+
+    args = parser.parse_args()
 
     dataset = CorpusData(
-        fpath=fpath,
-        fname=fname,
-        stop=stop
+        fpath=args.fpath,
+        fname=args.fname,
+        stop=args.stop
     )
-    embedding_matrix = id_to_glove(dataset.dict_yelp, abs_path)
 
+    embedding_matrix = id_to_glove(dataset.dict_yelp, args.abs_path)
     emb_t = torch.from_numpy(embedding_matrix)
-    emb = nn.Embedding(embedding_matrix.shape[0], embedding_matrix.shape[1])
-    emb.load_state_dict({'weight': emb_t})
-    model = RNN(emb_weights=emb_t, batch_size=batch_size, input_len=input_len)
+
+    model = RNN(
+        emb_weights=emb_t,
+        batch_size=args.batch_size,
+        input_len=args.input_len)
+
     model.load_weights()
-    model.to(device)
+
     optimizer = optim.Adam(
         model.parameters(),
         lr=0.0001,
         betas=(0.7, 0.99),
         weight_decay=1e-5
     )
+
     loss_function = nn.BCEWithLogitsLoss()
+
     scheduler = optim.lr_scheduler.CosineAnnealingLR(
         optimizer,
         100,
         eta_min=0,
         last_epoch=-1
     )
-    losses = fit(
-        model,
-        loss_function,
-        optimizer,
-        dataset,
-        batch_size,
-        n_epochs,
-        device=device
+
+    trainer = Trainer(
+        loss_function=loss_function,
+        optimizer=optimizer,
+        dataclass=dataset,
+        batch_size=args.batch_size,
+        epochs=args.n_epochs,
+        workers=4,
+        device=args.device
     )
-    torch.save(model.state_dict(), state_path)
-    print(f'model weights saved to : {state_path}')
+
+    trainer.fit(model)
+
+    torch.save(model.state_dict(), args.state_path)
+    print(f'model weights saved to: {args.state_path}')
+
+
+if __name__ == '__main__':
+    main()
