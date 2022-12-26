b3d01a09a5e48eae9580651c5f69aa40e1cc1cc3
Edward Yang <edwardpyang@gmail.com>
2022-12-26 15:06:47 -0500

    build files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="torch-sentiment",
-    version="0.41",
+    version="0.4.2",
     packages=find_packages(),
     install_requires=[
         "numpy",
