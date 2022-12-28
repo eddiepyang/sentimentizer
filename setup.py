0fdf16d826641222c16c5cfa24b277a2522a966e
Edward Yang <edwardpyang@gmail.com>
2022-12-28 13:20:48 -0500

    clean setup

--- setup.py
+++ setup.py
@@ -1,32 +1,5 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="torch-sentiment",
-    version="0.4.2",
-    packages=find_packages(),
-    install_requires=[
-        "numpy >= 1.24.0",
-        "pandas",
-        "torch",
-        "seaborn",
-        "spacy",
-        "scikit-learn",
-        "jsonlines",
-        "gensim",
-        "structlog",
-        "psutil",
-    ],
-    package_data={
-        # If any package contains *.txt or *.rst files, include them:
-        "": ["*.txt", "*.rst"],
-    },
-    entry_points={"console_scripts": ["run=torch_sentiment.workflows.driver:main"]},
-    # metadata to display on PyPI
-    author="Edward Yang",
-    author_email="edwardpyang@gmail.com",
-    description="straight forward rnn model",
-    project_urls={
-        "Source Code": "https://github.com/eddiepyang/torch-sentiment",
-    },
-    classifiers=["License :: OSI Approved :: Python Software Foundation License"],
+    packages=find_packages()
 )
