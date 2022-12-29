fa09d9136c14d29755a39f50222e87d5c1f0e84a
Edward Yang <edwardpyang@gmail.com>
2022-12-29 18:42:18 -0500

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ relative_root = Path(".").resolve()
 @pytest.fixture
 def rel_path():
     global relative_root
-    path = f"{relative_root}/torch_sentiment/tests/test_data/archive.zip"
+    path = f"{relative_root}/tests/test_data/archive.zip"
     return path
 
 if __name__=="__main__":
