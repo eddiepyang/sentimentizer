78a5a10d16a44b86f4d41d4e3d510afd3539b4cc
Edward Yang <edwardpyang@gmail.com>
2026-07-12 12:36:14 -0400

    fix asyncio run

--- tests/test_serve.py
+++ tests/test_serve.py
@@ -128,7 +128,7 @@ def _mock_deployment(predictor=None, ready=True, load_error=None):
 
 def _run(coro):
     """Run an async coroutine synchronously in tests."""
-    return asyncio.get_event_loop().run_until_complete(coro)
+    return asyncio.run(coro)
 
 
 def _mock_request():
